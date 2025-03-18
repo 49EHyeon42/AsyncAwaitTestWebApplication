a82799d95cb3cd52060834993db5d083399ebf84
DESKTOP-76RGINH\LG <thjo@mostisoft.com>
2025-03-18 14:00:54 +0900

    feat: K6 테스트 스크립트 추가

---
+++ test.js
@@ -0,0 +1,12 @@
+﻿import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+    vus: 10,           // 10명의 동시 사용자
+    duration: '30s',   // 30초 동안 실행
+};
+
+export default function () {
+    http.get('http://host.docker.internal:5000/api/test');  // 변경된 부분
+    sleep(1);
+}
